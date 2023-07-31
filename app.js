b689d07f91236540f48a13fb79218fd246b69702
Barbara Fajardo <barbarafajardo92@gmail.com>
2023-07-31 11:03:34 +0100

    added country data to json file

--- app.js
+++ app.js
@@ -13,29 +13,29 @@ app.use(logger)
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
+    res.send('Hello Countries Game!')
+})
 
-  
+
 app.get('/countries', (req, res) => {
     res.send(countries)
-  }) 
+})
 
 app.get('/countries/:id', (req, res) => {
     console.log(req.params)
     const idx = req.params.id
-    
-    const country = countries[idx -1]
-    if(!country){
-        res.status(404).json({message: 'Country with id ${idx} not found'})
+
+    const country = countries[idx - 1]
+    if (!country) {
+        res.status(404).json({ message: `Country with id ${idx} not found` })
     } else {
         res.send(country)
     }
-  }) 
+})
 
 
-app.post('/countries', (req,res) => {
-    
+app.post('/countries', (req, res) => {
+
     const country = req.body
     countries.push(country)
     res.status(201).send(country)
